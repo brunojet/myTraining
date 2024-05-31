584300fb678ce940609c636bc19264995940b5d6
Bruno Lima <brunojet@gmail.com>
2024-05-31 07:21:45 -0300

    Adding tabs.

--- front-end/apps-list-demo/appsController.js
+++ front-end/apps-list-demo/appsController.js
@@ -1,125 +1,51 @@
 angular.module('appsModule', [])
   .controller('appsController', function ($scope) {
     $scope.hasDebugLog = ($scope.$log && $scope.$log.debugEnabled());
-    $scope.data = {
-      "categories": [
-        "Todos",
-        "Jogos",
-        "Redes Sociais",
-        "Produtividade"
-      ],
-      "subCategories": [
-        {
-          "name": "Puzzle",
-          "category": "Jogos"
-        },
-        {
-          "name": "Aventura",
-          "category": "Jogos"
-        },
-        {
-          "name": "Simulação",
-          "category": "Jogos"
-        }
-      ],
-      "apps": [
-        {
-          "name": "Candy Crush",
-          "category": "Jogos",
-          "subCategory": ["Puzzle"],
-          "desenvolvedor": "King",
-          "mostDownloaded": true
-        },
-        {
-          "name": "Pokémon GO",
-          "category": "Jogos",
-          "subCategory": "Aventura",
-          "desenvolvedor": "Niantic"
-        },
-        {
-          "name": "Minecraft",
-          "category": "Jogos",
-          "subCategory": ["Simulação"],
-          "desenvolvedor": "Mojang",
-          "mostDownloaded": true
-        },
-        {
-          "name": "Facebook",
-          "category": "Redes Sociais",
-          "desenvolvedor": "Facebook, Inc."
-        },
-        {
-          "name": "Instagram",
-          "category": "Redes Sociais",
-          "desenvolvedor": "Facebook, Inc."
-        },
-        {
-          "name": "Twitter",
-          "category": "Redes Sociais",
-          "desenvolvedor": "Twitter, Inc."
-        },
-        {
-          "name": "Microsoft Office",
-          "category": "Produtividade",
-          "desenvolvedor": "Microsoft Corporation"
-        },
-        {
-          "name": "Trello",
-          "category": "Produtividade",
-          "desenvolvedor": "Atlassian"
-        },
-        {
-          "name": "Evernote",
-          "category": ["Produtividade"],
-          "desenvolvedor": "Evernote Corporation",
-          "mostDownloaded": true
-        },
-        {
-          "name": "LinkedIn",
-          "category": ["Redes Sociais", "Produtividade"],
-          "desenvolvedor": "LinkedIn, Inc."
-        }
-      ]
+    $scope.data = appData;
+    $scope.data.categories = $scope.data.categories.map(name => ({ name, expanded: false, highlights: false, mostDownloaded: false }));
+    $scope.page = { tab: 'highlights', showCategory: false, category: 'Todos', subCategory: null, subCategories: [] };
+
+    $scope.setTab = function (name) {
+      $scope.page.tab = name;
+      $scope.page.showCategory = ('recommended' === name);
     };
 
-    //Adjust data
-    $scope.data.categories = $scope.data.categories.map(name => ({ name, expanded: false, mostDownloaded: false }));
-    $scope.page = { category: 'Todos', subCategory: null, subCategories: [] };
-
-    isSameCategory = function (app, category) {
-      if (Array.isArray(app.category)) {
-        return app.category.includes(category.name);
+    hasDataInObject = function (object, data) {
+      if (Array.isArray(object)) {
+        return object.includes(data);
       } else {
-        return app.category === category.name;
+        return object === data;
       }
     }
 
     isSameSubCategory = function (app, subCategory) {
       if (subCategory) {
-        if (Array.isArray(app.subCategory)) {
-          return app.subCategory.includes(subCategory.name);
-        } else {
-          return app.subCategory === subCategory.name;
-        }
+        return hasDataInObject(app.subCategory, subCategory.name);
       }
 
       return true;
     }
 
-    mostDownloadedApps = function () {
-      var filteredApps = $scope.data.apps.filter(app => app.mostDownloaded);
+    classifiedApps = function () {
+      var filteredApps = $scope.data.apps.filter(app => app.tags);
 
       $scope.data.categories.forEach(category => {
         filteredApps.forEach(app => {
-          if (isSameCategory(app, category)) {
-            category.mostDownloaded = true;
+          if (hasDataInObject(app.category, category.name)) {
+            if (hasDataInObject(app.tags, 'highlights')) {
+              category.highlights = true;
+            }
+
+            if (hasDataInObject(app.tags, 'mostDownloaded')) {
+              category.mostDownloaded = true;
+            }
           }
         });
       });
     }
 
 
-    mostDownloadedApps();
+    classifiedApps();
 
     $scope.toggleCategory = function (category) {
       $scope.page.category.expanded = false;
@@ -152,11 +78,11 @@ angular.module('appsModule', [])
     $scope.filterApps = function (app) {
       if ($scope.page.category.name === 'Todos') {
         return true;
-      } else if (isSameCategory(app, $scope.page.category)) {
+      } else if (hasDataInObject(app.category, $scope.page.category.name)) {
         if ($scope.page.subCategory?.name === 'Mais baixados') {
-          return app.mostDownloaded;
+          return hasDataInObject(app.tags, 'mostDownloaded');
         } else {
-          return isSameSubCategory(app, $scope.page.subCategory);
+          return isSameSubCategory(app.category, $scope.page.subCategory);
         }
       } else {
         return false;
---
+++ front-end/apps-list-demo/appsData.js
@@ -0,0 +1,80 @@
+var appData = {
+    "categories": [
+        "Todos",
+        "Jogos",
+        "Redes Sociais",
+        "Produtividade"
+    ],
+    "subCategories": [
+        {
+            "name": "Puzzle",
+            "category": "Jogos"
+        },
+        {
+            "name": "Aventura",
+            "category": "Jogos"
+        },
+        {
+            "name": "Simulação",
+            "category": "Jogos"
+        }
+    ],
+    "apps": [
+        {
+            "name": "Candy Crush",
+            "category": "Jogos",
+            "subCategory": ["Puzzle"],
+            "desenvolvedor": "King",
+            "tags": ["highlights", "mostDownloaded"]
+        },
+        {
+            "name": "Pokémon GO",
+            "category": "Jogos",
+            "subCategory": "Aventura",
+            "desenvolvedor": "Niantic"
+        },
+        {
+            "name": "Minecraft",
+            "category": "Jogos",
+            "subCategory": ["Simulação"],
+            "desenvolvedor": "Mojang",
+            "tags": ["highlights", "mostDownloaded"]
+        },
+        {
+            "name": "Facebook",
+            "category": "Redes Sociais",
+            "desenvolvedor": "Facebook, Inc."
+        },
+        {
+            "name": "Instagram",
+            "category": "Redes Sociais",
+            "desenvolvedor": "Facebook, Inc."
+        },
+        {
+            "name": "Twitter",
+            "category": "Redes Sociais",
+            "desenvolvedor": "Twitter, Inc."
+        },
+        {
+            "name": "Microsoft Office",
+            "category": "Produtividade",
+            "desenvolvedor": "Microsoft Corporation"
+        },
+        {
+            "name": "Trello",
+            "category": "Produtividade",
+            "desenvolvedor": "Atlassian"
+        },
+        {
+            "name": "Evernote",
+            "category": ["Produtividade"],
+            "desenvolvedor": "Evernote Corporation",
+            "tags": ["mostDownloaded"]
+        },
+        {
+            "name": "LinkedIn",
+            "category": ["Redes Sociais", "Produtividade"],
+            "desenvolvedor": "LinkedIn, Inc."
+        }
+    ]
+};
