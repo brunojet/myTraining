c064d8da5abf11b5f27d2549da64ac786575dbaa
Bruno Lima <brunojet@gmail.com>
2024-06-01 16:42:24 -0300

    no message

--- front-end/apps-list-demo/appsData.js
+++ front-end/apps-list-demo/appsData.js
@@ -20,68 +20,71 @@ var appData = {
     "apps": [
         {
             "name": "Candy Crush",
+            "developer": "King",
+            "icon": "candy-crush.svg",
             "category": C_JOGOS,
             "subCategory": [SC_PUZZLE],
-            "developer": "King",
             "tags": [T_HIGHLIGHTS, T_MOST_DOWNLOADED],
-            "svg": "M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"
         },
         {
             "name": "Pokémon GO",
-            "category": C_JOGOS,
-            "subCategory": SC_AVENTURA,
             "developer": "Niantic",
-            "svg": "M50,10 C70,10 90,30 90,50 C90,70 70,90 50,90 C30,90 10,70 10,50 C10,30 30,10 50,10 M50,30 C40,30 30,40 30,50 C30,60 40,70 50,70 C60,70 70,60 70,50 C70,40 60,30 50,30 M50,40 C55,40 60,45 60,50 C60,55 55,60 50,60 C45,60 40,55 40,50 C40,45 45,40 50,40 M50,45 C52,45 55,48 55,50 C55,52 52,55 50,55 C48,55 45,52 45,50 C45,48 48,45 50,45"
+            "icon": "pokemon-go.svg",
+            "category": C_JOGOS,
+            "subCategory": SC_AVENTURA
         },
         {
             "name": "Minecraft",
+            "developer": "Mojang",
+            "icon": "minecraft.svg",
             "category": C_JOGOS,
             "subCategory": [SC_SIMULACAO],
-            "developer": "Mojang",
-            "tags": [T_HIGHLIGHTS, T_MOST_DOWNLOADED, T_RECOMMENDED],
-            "svg": "M10 10l80 0 0 80 -80 0z M30 10l0 80 M50 10l0 80 M70 10l0 80 M10 30l80 0 M10 50l80 0 M10 70l80 0"
+            "tags": [T_HIGHLIGHTS, T_MOST_DOWNLOADED, T_RECOMMENDED]
         },
         {
             "name": "Facebook",
             "category": C_REDES_SOCIAIS,
-            "developer": "Facebook, Inc."
+            "developer": "Facebook, Inc.",
+            "icon": "facebook.svg"
         },
         {
             "name": "Instagram",
             "category": C_REDES_SOCIAIS,
             "developer": "Facebook, Inc.",
-            tags: T_RECOMMENDED
+            "icon": "instagram.svg",
+            "tags": T_RECOMMENDED
         },
         {
             "name": "Twitter",
             "category": C_REDES_SOCIAIS,
-            "developer": "Twitter, Inc."
+            "developer": "Twitter, Inc.",
+            "icon": "twitter.svg"
         },
         {
             "name": "Microsoft Office",
             "category": C_PRODUTIVIDADE,
             "developer": "Microsoft Corporation",
-            "tags": T_RECOMMENDED,
-            "svg": "M 60 10 L 60 90 L 90 90 L 90 10 Z"
+            "icon": "microsoft-office.svg",
+            "tags": T_RECOMMENDED
         },
         {
             "name": "Trello",
             "category": C_PRODUTIVIDADE,
             "developer": "Atlassian",
-            "svg": "M 10 10 L 10 90 L 90 90 L 90 10 Z"
+            "icon": "trello.svg"
         },
         {
             "name": "Evernote",
             "category": [C_PRODUTIVIDADE],
             "developer": "Evernote Corporation",
-            "tags": [T_MOST_DOWNLOADED],
-            "svg": "M 10 10 L 90 10 L 50 50 Z"
+            "icon": "evernote.svg",
+            "tags": [T_MOST_DOWNLOADED]
         },
         {
             "name": "LinkedIn",
             "category": [C_REDES_SOCIAIS, C_PRODUTIVIDADE],
             "developer": "LinkedIn, Inc.",
-            "svg": "M 10 90 L 90 90 L 50 10 Z"
+            "icon": "linkedin.svg"
         }
     ]
 };
